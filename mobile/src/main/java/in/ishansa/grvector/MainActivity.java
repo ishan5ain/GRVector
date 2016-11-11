8d77aa87377b05aeea26ef85c6eef836cf5d5918
Ishan Sain <ishansain27@gmail.com>
2016-11-11 20:37:34 +0530

    init 1

---
+++ mobile/src/main/java/in/ishansa/grvector/MainActivity.java
@@ -0,0 +1,13 @@
+package in.ishansa.grvector;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
